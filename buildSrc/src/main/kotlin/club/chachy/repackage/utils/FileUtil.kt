44baefbb4e26b1c8aa83662f0a05964d31f1ed2b
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-11 18:12:40 +0100

    Fix relocation logic
    I forgot we don't need this confusing way because we don't do minecraft stuff in here :pensive:

--- buildSrc/src/main/kotlin/club/chachy/repackage/utils/FileUtil.kt
+++
@@ -1,5 +0,0 @@
-package club.chachy.repackage.utils
-
-import java.io.File
-
-fun copyFile(`in`: File, out: File) = out.writeBytes(`in`.readBytes())
--- buildSrc/src/main/kotlin/club/chachy/repackage/utils/ZipFileExt.kt
+++
@@ -1,7 +0,0 @@
-package club.chachy.repackage.utils
-
-import java.util.zip.ZipEntry
-import java.util.zip.ZipFile
-
-fun ZipEntry.byteArray(file: ZipFile) = file.getInputStream(this)
-    ?.use { it.readBytes() }
