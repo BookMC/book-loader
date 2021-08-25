b9d69bb6afdf0a9aea904642738885b25531c475
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-25 21:46:37 +0100

    Try to fix what would be re-entrant transformers not transforming

--- src/main/java/org/bookmc/loader/impl/launch/transform/mixin/MixinServiceQuilt.java
+++ src/main/java/org/bookmc/loader/impl/launch/transform/mixin/MixinServiceQuilt.java
@@ -22,13 +22,13 @@ public class MixinServiceQuilt implements IMixinService, IClassProvider, IClassB
 
     @Override
     public ClassNode getClassNode(String name) {
-        return getClassNode(name, false);
+        return Launcher.getClassNode(name, false, 0);
     }
 
     @Override
     public ClassNode getClassNode(String name, boolean runTransformers) {
         // We use 0 for our flags to avoid re-entrant transformer errors
-        return Launcher.getClassNode(name, runTransformers, 0);
+        return Launcher.getClassNode(name, true, 0);
     }
 
     @Override
